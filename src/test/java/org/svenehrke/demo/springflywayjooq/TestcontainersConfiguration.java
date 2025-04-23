6f815f13eae6615dc71fb8b353c8707a6c72c2bc
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2025-04-23 19:40:46 +0200

    made port of postgres container fixed first flyway script

--- src/test/java/org/svenehrke/demo/springflywayjooq/TestcontainersConfiguration.java
+++ src/test/java/org/svenehrke/demo/springflywayjooq/TestcontainersConfiguration.java
@@ -1,10 +1,13 @@
 package org.svenehrke.demo.springflywayjooq;
 
+import com.github.dockerjava.api.model.ExposedPort;
+import com.github.dockerjava.api.model.HostConfig;
+import com.github.dockerjava.api.model.PortBinding;
+import com.github.dockerjava.api.model.Ports;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
@@ -12,7 +15,21 @@ class TestcontainersConfiguration {
 	@Bean
 	@ServiceConnection
 	PostgreSQLContainer<?> postgresContainer() {
-		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+		int containerPort = 5432 ;
+		int localPort = 5432 ;
+		var container = new PostgreSQLContainer<>("postgres:latest")
+			.withUsername("postgres")
+			.withPassword("postgres")
+			.withReuse(false)
+			.withExposedPorts(5432, 5432)
+			.withCreateContainerCmdModifier(cmd -> cmd.withHostConfig(
+				new HostConfig()
+					.withPortBindings(
+						new PortBinding(
+							Ports.Binding.bindPort(localPort),
+							new ExposedPort(containerPort))
+					))
+			);
+		return container;
 	}
-
 }
